332729786b212c68c3ec66c4cf758e3ac24f2e16
Armann-Sch <armannsch@gmail.com>
2024-08-31 21:51:30 +0000

    Dæmi 1

--- verkefni2/daemi1/gasket1.js
+++ verkefni2/daemi1/gasket1.js
@@ -29,7 +29,7 @@ window.onload = function init()
 
     var u = add( vertices[0], vertices[1] );
     var v = add( vertices[0], vertices[2] );
-    //var p = scale( 1, add( u, v ) );
+    //var p = scale( 0.25, add( u, v ) );
     var p = [100, 100];
     console.log(p);
     // And, add our initial point into our array of points
@@ -41,7 +41,11 @@ window.onload = function init()
     // last point and a randomly chosen vertex
 
     for ( var i = 0; points.length < NumPoints; ++i ) {
-        var j = Math.floor(Math.random() * 3);
+        var j = 0;
+        var rand = Math.random();
+        if (rand < 0.9) {
+            j = Math.floor(rand * 2) + 1;
+        }
         p = add( points[i], vertices[j] );
         p = scale( 0.5, p );
         points.push( p );
